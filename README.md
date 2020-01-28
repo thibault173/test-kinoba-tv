be7dc7dd092d12f67ec43308b345c7217b2ac4f5
thibault173 <thibault.villard@gmail.com>
2020-01-28 17:59:02 +0100

    Adding map

--- README.md
+++ README.md
@@ -30,4 +30,11 @@
 
   rails db:migrate
 
++ Keys
+
+  Create a .env file in root file
+  Ask team for keys :
+
+  MAPBOX_API_KEY
+
 Rails app generated inspired from [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,11 @@ class PagesController < ApplicationController
     @end_id = (@page + 1) * 100
     @tribe_members = TribeMember.includes(:ancestor).where(id: @start_id..@end_id)
     @number_of_results = TribeMember.count
+    @markers = @tribe_members.map do |tribe_member|
+      {
+        lat: tribe_member.latitude,
+        lng: tribe_member.longitude
+      }
+    end
   end
 end
