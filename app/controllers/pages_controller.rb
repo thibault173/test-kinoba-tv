f5959bd25042530266e99caae489fac24a7528b5
thibault173 <thibault.villard@gmail.com>
2020-01-28 16:31:59 +0100

    First index page with all results

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    @page = params[:page].to_i || 0
+    @start_id = @page * 100 + 1
+    @end_id = (@page + 1) * 100
+    @tribe_members = TribeMember.includes(:ancestor).where(id: @start_id..@end_id)
+    @number_of_results = TribeMember.count
   end
 end
